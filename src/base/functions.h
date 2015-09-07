ce717ea5ff234af932c78288c72be9565f39d713
Takuya Akiba <t.akiba.65536@gmail.com>
2015-09-07 15:45:38 +0900

    union find

--- src/base/functions.h
+++ src/base/functions.h
@@ -3,7 +3,10 @@
 
 
 namespace agl {
-// みさわさんに教えてもらったテク：
+// Cygwin/MinGW では to_string が定義されていないバグがある．
+// http://stackoverflow.com/questions/12975341/to-string-is-not-a-member-of-std-says-so-g
+// http://gcc.gnu.org/bugzilla/show_bug.cgi?id=52015
+// これはみさわさんに教えてもらったテク：
 // template<typename ...> static inline int getchar_unlocked(void){ return getchar(); }
 
 template <typename T, typename ...>
