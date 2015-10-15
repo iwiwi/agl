1ff8141463f539146fc0646e903c8b45048819cb
Takuya Akiba <t.akiba.65536@gmail.com>
2015-10-15 19:20:01 +0900

    index のクラスをファイルに分離

--- src/graph/basic_graph.h
+++ src/graph/basic_graph.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "base/base.h"
 #include "direction.h"
+#include "graph_index_interface.h"
 #include <vector>
 #include <cstdint>
 #include <fstream>
@@ -67,25 +68,6 @@ class undirected_neighbor_range {
 
  private:
   iterator_type i_, n_;
-};
-
-template<typename GraphType>
-class graph_index_interface {
- public:
-  virtual ~graph_index_interface() {}
-  virtual void construct(const GraphType &g) = 0;
-};
-
-template<typename GraphType>
-class graph_dynamic_index_interface : public graph_index_interface<GraphType> {
- public:
-  using E = typename GraphType::E;
-  virtual ~graph_dynamic_index_interface() {}
-
-  virtual void add_edge(const GraphType &g, V v_from, const E &e) = 0;
-  virtual void remove_edge(const GraphType &g, V v_from, V v_to) = 0;
-  virtual void add_vertices(const GraphType &g, V old_num_vertices) = 0;
-  virtual void remove_vertices(const GraphType &g, V old_num_vertices) = 0;
 };
 
 template<typename EdgeType>
---
+++ src/graph/graph_index_interface.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace agl {
+template<typename GraphType>
+class graph_index_interface {
+ public:
+  virtual ~graph_index_interface() {}
+  virtual void construct(const GraphType &g) = 0;
+};
+
+template<typename GraphType>
+class graph_dynamic_index_interface : public graph_index_interface<GraphType> {
+ public:
+  using E = typename GraphType::E;
+  virtual ~graph_dynamic_index_interface() {}
+
+  virtual void add_edge(const GraphType &g, V v_from, const E &e) = 0;
+  virtual void remove_edge(const GraphType &g, V v_from, V v_to) = 0;
+  virtual void add_vertices(const GraphType &g, V old_num_vertices) = 0;
+  virtual void remove_vertices(const GraphType &g, V old_num_vertices) = 0;
+};
+}
