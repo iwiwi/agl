9d81673bc6ba784750a3c751e31eb049fbc953f8
Takuya Akiba <t.akiba.65536@gmail.com>
2015-10-21 17:17:43 +0900

    dynamic evaluation

---
+++ src/distance_sketch/cui/distance_sketch_dynamic_performance_main.cc
@@ -0,0 +1,11 @@
+#include "easy_cui.h"
+#include "distance_sketch/distance_sketch.h"
+
+int main(int argc, char **argv) {
+  dynamic_index_evaluation_scenario<G> s;
+  s.initial_graph = easy_cui_init(argc, argv);
+  s.add_workload_edge_addition_and_removal_random(100);
+
+  distance_sketch::dynamic_all_distances_sketches ads();
+  s.evaluate((dynamic_graph_index_interface<G>*)&ads);
+}
