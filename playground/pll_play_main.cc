5c025777400ff658bc3b742171bd93ec0c8045cc
kenkoooo <kenkou.n@gmail.com>
2015-11-05 16:51:34 +0900

    こいつを移植すればよいはず

--- playground/pll_play_main.cc
+++ playground/pll_play_main.cc
@@ -6,15 +6,14 @@ using namespace agl;
 int main() {
   unweighted_edge_list es = generate_grid(3, 3);
   G g(es);
-  pretty_print(g);
 
   {
     dynamic_pruned_landmark_labeling<> dpll;
     dpll.construct(g);
     cout << dpll.query_distance(g, 0, 5) << endl;
+  pretty_print(g);
     // UPDATE
     dpll.add_edge(g, 0, 5);
-    es.emplace_back(0, 5);
     cout << dpll.query_distance(g, 0, 5) << endl;
   }
 
