3efc2341f22a8d06db13a2884b918a6a51e5dd47
kenkoooo <kenkou.n@gmail.com>
2015-11-07 17:40:00 +0900

    もしかしたら逆辺追加するだけなんじゃないか

--- src/shortest_path/dynamic_pruned_landmark_labeling.h
+++ src/shortest_path/dynamic_pruned_landmark_labeling.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "graph/graph.h"
 #include "graph/graph_index_interface.h"
+#include <malloc.h>
 
 namespace agl {
 template <size_t kNumBitParallelRoots = 16>
@@ -27,37 +28,101 @@ class dynamic_pruned_landmark_labeling
   }
 
  private:
+  const uint8_t INF8 = 100;  // For unreachable pairs
+  const uint32_t INF32 = std::numeric_limits<int32_t>::max();  // For sentinel
+
   struct index_t {
-    uint32_t *spt_v;
-    uint8_t *spt_d;
+    uint32_t *spt_vertex;
+    uint8_t *spt_dist;
     uint32_t spt_l;
   } __attribute__((aligned(64)));  // Aligned for cache lines
 
-  index_t *index_;
-  std::vector<std::vector<V>> adj_[2];
+  index_t *index_, *index_inv_;
+  std::vector<std::vector<V>> adj_, adj_inv_;
+  V num_vertices_;
+  E num_edges_;
 };
 
 template <size_t kNumBitParallelRoots>
 void dynamic_pruned_landmark_labeling<kNumBitParallelRoots>::construct(
     const G &g) {
-  size_t num_edge = g.num_edges();
-  size_t num_v = g.num_vertices();
+  E &num_e = num_edges_;
+  V &num_v = num_vertices_;
+  num_e = g.num_edges();
+  num_v = g.num_vertices();
   unweighted_edge_list es = g.edge_list();
 
-  adj_[0].resize(num_v);
-  adj_[1].resize(num_v);
-  for (size_t i = 0; i < num_edge; ++i) {
+  adj_.resize(num_v);
+  adj_inv_.resize(num_v);
+  for (size_t i = 0; i < num_v; ++i) {
     V from = es[i].first, to = es[i].second;
-    adj_[0][from].push_back(to);
-    adj_[1][to].push_back(from);
+    adj_[from].push_back(to);
+    adj_inv_[to].push_back(from);
   }
 
-  // pruned bfs
+  // Initialize Index
+
+  index_ = (index_t *)memalign(64, num_v * sizeof(index_t));
+  index_inv_ = (index_t *)memalign(64, num_v * sizeof(index_t));
+  if (index_ == NULL || index_inv_ == NULL) {
+    num_v = 0;
+    return;
+  }
+  for (V v = 0; v < num_v; ++v) {
+    index_[v].spt_vertex = NULL;
+    index_[v].spt_dist = NULL;
+    index_[v].spt_l = 0;
+    index_inv_[v].spt_vertex = NULL;
+    index_inv_[v].spt_dist = NULL;
+    index_inv_[v].spt_l = 0;
+  }
+
+  // Pruned bfs
+  {
+    std::vector<W> tmp_dist(num_v);
+    std::vector<V> queue(num_v);
+    for (V v = 0; v < num_v; ++v) {
+      int queue_tail = 0, queue_head = 0;
+      queue[queue_tail++] = v;
+
+      fill(tmp_dist.begin(), tmp_dist.end(), INF8);
+      tmp_dist[v] = 0;
+      // L<-L
+      while (queue_head < queue_tail) {
+        V u = queue[queue_head++];
+        
+      }
+    }
+  }
 }
 
 template <size_t kNumBitParallelRoots>
 W dynamic_pruned_landmark_labeling<kNumBitParallelRoots>::query_distance(
-    const G &g, V v_from, V v_to) {}
+    const G &g, V v_from, V v_to) {
+  if (v_from == v_to) return 0;
+  if (v_from >= num_vertices_ || v_to >= num_vertices_) return kInfW;
+
+  const index_t &idx_from = index_[v_from];
+  const index_t &idx_to = index_inv_[v_to];
+  W dist = INF8;
+
+  for (int i1 = 0, i2 = 0;;) {
+    V v1 = idx_from.spt_vertex[i1], v2 = idx_to.spt_vertex[i2];
+    if (v1 == v2) {
+      if (v1 == INF32) break;  // Sentinel
+      W tmp_dist = idx_from.spt_dist[i1] + idx_to.spt_dist[i2];
+      if (tmp_dist < dist) dist = tmp_dist;
+      ++i1;
+      ++i2;
+    } else {
+      i1 += v1 < v2 ? 1 : 0;
+      i2 += v1 > v2 ? 1 : 0;
+    }
+  }
+
+  if (dist >= INF8 - 2) dist = kInfW;
+  return dist;
+}
 
 template <size_t kNumBitParallelRoots>
 void dynamic_pruned_landmark_labeling<kNumBitParallelRoots>::add_edge(
