4a075bafc7eba267a6df68a1281d295d9e4457fa
kenkoooo <kenkou.n@gmail.com>
2015-11-25 13:25:37 +0900

    リファクタリング

--- playground/box_cover.cc
+++ playground/box_cover.cc
@@ -4,74 +4,74 @@ using namespace std;
 vector<V> box_cover_memb(const G &g, W radius) {
   V num_v = g.num_vertices();
   vector<vector<pair<V, W>>> node_lists;
-  vector<pair<size_t, V>> center_candidates;
-  for (V pv = 0; pv < num_v; ++pv) {
-    queue<pair<V, W>> que;
-    vector<bool> vis(num_v, false);
-    vector<pair<V, W>> nodes;
+  map<size_t, set<V>> excluded_mass_map;
+  {
+    vector<pair<size_t, V>> center_candidates;
+    for (V pv = 0; pv < num_v; ++pv) {
+      queue<pair<V, W>> que;
+      vector<bool> vis(num_v, false);
+      vector<pair<V, W>> nodes;
 
-    que.push(make_pair(pv, 0));
-    nodes.push_back(make_pair(pv, 0));
-    vis[pv] = true;
+      que.push(make_pair(pv, 0));
+      nodes.push_back(make_pair(pv, 0));
+      vis[pv] = true;
 
-    while (!que.empty()) {
-      V v = que.front().first;
-      W dist = que.front().second;
-      que.pop();
-      if (dist >= radius) continue;
-      for (V u : g.neighbors(v)) {
-        if (vis[u]) continue;
-        que.push(make_pair(u, dist + 1));
-        nodes.push_back(make_pair(u, dist + 1));
-        vis[u] = true;
+      while (!que.empty()) {
+        V v = que.front().first;
+        W dist = que.front().second;
+        que.pop();
+        if (dist >= radius) continue;
+        for (V u : g.neighbors(v)) {
+          if (vis[u]) continue;
+          que.push(make_pair(u, dist + 1));
+          nodes.push_back(make_pair(u, dist + 1));
+          vis[u] = true;
+        }
       }
+
+      node_lists.push_back(nodes);
+      center_candidates.push_back(make_pair(nodes.size(), pv));
     }
-
-    node_lists.push_back(nodes);
-    center_candidates.push_back(make_pair(nodes.size(), pv));
-  }
-
-  map<size_t, set<V>> excluded_mass_of_non_centers;
-  for (pair<size_t, V> p : center_candidates) {
-    excluded_mass_of_non_centers[p.first].insert(p.second);
+    for (pair<size_t, V> p : center_candidates) {
+      excluded_mass_map[p.first].insert(p.second);
+    }
   }
 
   set<V> covered_nodes;
   set<V> center_nodes;
-  vector<W> central_distance_of_node(num_v, num_v);
+  vector<W> central_distance(num_v, num_v);
   while (covered_nodes.size() < (size_t)num_v) {
     V center_node_found;
     while (true) {
       V node, maximum_key;
       while (true) {
-        maximum_key = excluded_mass_of_non_centers.rbegin()->first;
-        set<V> &nodes = excluded_mass_of_non_centers.rbegin()->second;
+        maximum_key = excluded_mass_map.rbegin()->first;
+        set<V> &nodes = excluded_mass_map.rbegin()->second;
         auto it = nodes.begin();
         advance(it, agl::random(nodes.size()));
         node = *it;
         if (center_nodes.find(node) != center_nodes.end()) {
           nodes.erase(it);
-          if (nodes.empty()) excluded_mass_of_non_centers.erase(maximum_key);
+          if (nodes.empty()) excluded_mass_map.erase(maximum_key);
         } else {
           break;
         }
       }
-      
-      vector<pair<V, W>> &bfs = node_lists[node];
-      V excluded_mass_of_node = 0;
-      for (pair<V, W> p : bfs) {
+
+      V mass = 0;
+      for (pair<V, W> p : node_lists[node]) {
         if (covered_nodes.find(p.first) == covered_nodes.end()) {
-          excluded_mass_of_node++;
+          mass++;
         }
       }
-      excluded_mass_of_non_centers[maximum_key].erase(node);
-      if (excluded_mass_of_non_centers[maximum_key].empty())
-        excluded_mass_of_non_centers.erase(maximum_key);
-      if (excluded_mass_of_node == maximum_key) {
+      excluded_mass_map[maximum_key].erase(node);
+      if (excluded_mass_map[maximum_key].empty())
+        excluded_mass_map.erase(maximum_key);
+      if (mass == maximum_key) {
         center_node_found = node;
         break;
       } else {
-        excluded_mass_of_non_centers[excluded_mass_of_node].insert(node);
+        excluded_mass_map[mass].insert(node);
       }
     }
     center_nodes.insert(center_node_found);
@@ -79,9 +79,7 @@ vector<V> box_cover_memb(const G &g, W radius) {
       V i = p.first;
       W d = p.second;
       covered_nodes.insert(i);
-      if (central_distance_of_node[i] > d) {
-        central_distance_of_node[i] = d;
-      }
+      central_distance[i] = max(central_distance[i], d);
     }
   }
 
