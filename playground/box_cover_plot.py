ca2c80901d6f549c99a1f7051ec2ef733b97188c
kenkoooo <kenkou.n@gmail.com>
2015-11-26 11:11:20 +0900

    それっぽいフィッティングが書けるようになった

--- playground/box_cover_plot.py
+++ playground/box_cover_plot.py
@@ -1,6 +1,9 @@
+# coding: UTF-8
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 import seaborn as sns
 import json
 import sys
@@ -17,9 +20,37 @@ if __name__ == '__main__':
         y.append(boxSize[i])
         if boxSize[i] == 1:
             break
+
+    x = np.array(x)
+    y = np.array(y)
+    nsample = x.size
+    lnx = []
+    lny = []
+    for i in range(0, x.size):
+        lnx.append(np.log10(x[i]))
+        lny.append(np.log10(y[i]))
+
+    lnx = np.array(lnx)
+    lny = np.array(lny)
+
+    # おまじない (後で解説)
+    X = np.column_stack((np.repeat(1, nsample), lnx))
+    # 回帰実行
+    model = sm.OLS(lny, X)
+    results = model.fit()
+
+    # 結果の概要を表示
+    print results.summary()
+
+    # パラメータの推定値を取得
+    a, b = results.params
+
+    # プロットを表示
+    plt.plot(x, y, 'o')
+    plt.plot(x, (x**b) * (10**a))
+
     plt.xscale("log")
     plt.yscale("log")
     plt.xlim(xmin=0.5)
     plt.xlim(xmax=25)
-    plt.scatter(x, y)
     plt.savefig(sys.argv[2])
