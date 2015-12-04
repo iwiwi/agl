4ced9db313674e52dbde0d3e1445844952471131
math <314@live.jp>
2015-12-04 14:34:11 +0900

    バイナリからグラフを作成メソッドをplaygroundから graph/io.hに移動、テスト作成

--- src/graph/io.h
+++ src/graph/io.h
@@ -1,6 +1,19 @@
 #pragma once
 #include "base/base.h"
+#include "base/io.h"
 #include "graph.h"
+
+namespace {
+  //set the end line code(regardless of the os)
+  const std::string kEndLine = "\n";
+  const std::string kMagic = "AGL_BINARY";
+  const std::string kVersion = "0.01";
+
+  // http://d.hatena.ne.jp/osyo-manga/20120211/1328922379
+  extern void* enabler;
+  template<bool B, typename T = void>
+  using enabler_if = typename std::enable_if<B, T>::type*&;
+}
 
 namespace agl {
 template<typename GraphType = G>
@@ -81,5 +94,115 @@ void pretty_print(const GraphType &g, std::ostream &os = std::cerr) {
     }
   }
   os << "=========" << std::endl;
+}
+
+//format
+template<typename WeightType,
+  enabler_if<std::is_integral<WeightType>::value> = enabler >
+std::string graph_binary_format_weight() {
+  return "weight=int,weight_size=" + to_string(sizeof(WeightType));
+}
+template<typename WeightType,
+  enabler_if<std::is_floating_point<WeightType>::value> = enabler >
+std::string graph_binary_format_weight() {
+  return "weight=float,weight_size=" + to_string(sizeof(WeightType));
+}
+
+template<typename GraphType>
+const std::string graph_binary_format() {
+  using decayed_gragh_type = typename std::decay<GraphType>::type;
+  using weight_type = typename decayed_gragh_type::W;
+
+  return graph_binary_format_weight<weight_type>();
+}
+
+// write_binary
+template<typename EdgeType,
+  enabler_if<std::is_pod<EdgeType>::value> = enabler >
+void write_edge_binary(std::ostream& os, const EdgeType& e) {
+  write_binary(os, e);
+}
+
+template<typename GraphType>
+void write_graph_binary(const GraphType &g, std::ostream &os = std::cout) {
+  //header
+  os << kMagic << kEndLine << kVersion << kEndLine << graph_binary_format<GraphType>() << kEndLine;
+
+  //body
+  write_binary(os, g.num_vertices());
+  write_binary(os, g.num_edges());
+  for(V v = 0; v < g.num_vertices(); v++) {
+    write_binary(os, g.degree(v));
+    for(std::size_t i = 0; i < g.degree(v); i++) {
+      write_binary(os, g.edge(v,i));
+    }
+  }
+  os.flush();
+}
+
+template<typename GraphType = G>
+void write_graph_binary(const GraphType &g, const char *filename) {
+  if (strcmp(filename, "-") == 0) {
+    write_graph_binary(g, std::cout);
+  } else {
+    std::ofstream ofs(filename);
+    CHECK_PERROR(ofs);
+    write_graph_binary(g, ofs);
+  }
+}
+
+//reader binary
+template<typename EdgeType,
+  enabler_if<std::is_pod<EdgeType>::value> = enabler >
+EdgeType read_edge_binary(std::istream& is) {
+  EdgeType e;
+  read_binary(is, &e);
+  return e;
+}
+
+template<typename GraphType = G>
+GraphType read_graph_binary(std::istream &is = std::cin) {
+  //header
+  std::string magic,version,format;
+  std::getline(is, magic);
+  std::getline(is, version);
+  std::getline(is, format);
+
+  CHECK_MSG(magic == kMagic, "Invalid file magic.");
+  CHECK_MSG(version == kVersion, "Invalid file version.");
+  CHECK_MSG(format == graph_binary_format<GraphType>(), "Invalid file format.");
+
+  //body
+  typename GraphType::V num_vertices;
+  std::size_t num_edges;
+  read_binary(is, &num_vertices);
+  read_binary(is, &num_edges);
+
+  std::vector<std::vector<typename GraphType::E>> edges(num_vertices);
+
+  for(V v = 0; v < num_vertices; v++) {
+    std::size_t degree;
+    read_binary(is, &degree);
+    edges[v].reserve(degree);
+    for(std::size_t i = 0; i < degree; i++) {
+      edges[v].emplace_back(read_edge_binary<typename GraphType::E>(is));
+    }
+  }
+
+  GraphType deserialized_graph;
+  deserialized_graph.assign(std::move(edges));
+  return deserialized_graph;
+}
+
+template<typename GraphType = G>
+GraphType read_graph_binary(const char *filename) {
+  if (strcmp(filename, "-") == 0) {
+    return read_graph_binary<GraphType>(std::cin);
+  } else {
+    std::ifstream ifs(filename);
+    ifs.sync_with_stdio(false);
+    CHECK_PERROR(ifs);
+    return read_graph_binary<GraphType>(ifs);
+  }
 }
 }  // namespace agl
--- src/graph/io_test.cc
+++ src/graph/io_test.cc
@@ -4,6 +4,40 @@ using namespace std;
 using namespace agl;
 using testing::Types;
 
+
+namespace {
+typedef Types<unweighted_graph, weighted_graph<int>, weighted_graph<double> > GraphTestTypes;
+
+template<typename T>
+class graph_io_test : public testing::Test {};
+TYPED_TEST_CASE(graph_io_test, GraphTestTypes);
+
+TYPED_TEST(graph_io_test, binary) {
+  using GraphType = TypeParam;
+  GraphType g(add_random_weight<GraphType>(generate_erdos_renyi(5, 2)));
+  pretty_print(g);
+
+  std::ostringstream oss (std::stringstream::binary);
+  write_graph_binary(g, oss);
+
+  std::istringstream iss(oss.str(), std::stringstream::binary);
+  auto g2 = read_graph_binary<decltype(g)>(iss);
+
+  CHECK(g.num_vertices() == g2.num_vertices());
+  for(auto v : g.vertices()) {
+    CHECK(g.degree(v) == g2.degree(v));
+    for(size_t i = 0; i < g.degree(v); i++) {
+      const auto& e1 = g.edge(v,i);
+      const auto& e2 = g2.edge(v,i);
+      CHECK(to(e1) == to(e2));
+      CHECK(weight(e1) == weight(e2));
+    }
+  }
+}
+
+} // namespace
+
+
 /*
 namespace {
 
