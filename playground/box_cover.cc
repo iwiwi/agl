9b480661100afd20a24a35b22a9e0b3375cfbc9d
kenkoooo <kenkou.n@gmail.com>
2015-12-09 20:42:55 +0900

    バグっている Select-Greedily

--- playground/box_cover.cc
+++ playground/box_cover.cc
@@ -475,8 +475,127 @@ double estimated_cardinality(const G &g, const set<V> &X, const int k) {
   return (k - 1);
 }
 
-void select_greedily(const G &g, const vector<vector<V>> &X, vector<V> &centers,
-                     vector<bool> &centered, const int k) {}
+void select_greedily(const G &g, const vector<vector<V>> &X,
+                     const vector<V> inv, vector<V> &centers,
+                     vector<bool> &centered, const int k) {
+  assert(false);
+  // Initialization
+  V num_v = g.num_vertices();
+  set<V> Xs;
+  priority_queue<pair<V, V>> que[2];
+  multimap<V, V> T;
+  vector<int> kp(num_v, k);
+  vector<V> keys(num_v);
+  vector<bool> is_type1(num_v, true);
+
+  for (V p = 0; p < num_v; ++p) {
+    V kth = X[p].size() == k ? X[p][k - 1] : k - 1;
+    que[0].push(make_pair(kth, p));
+    T.insert(make_pair(kth, p));
+    keys[p] = kth;
+  }
+
+  // Main loop
+  while (estimated_cardinality(g, Xs, k) < max(num_v, k - 1)) {
+    // Selection
+    V selected_v = -1;
+    double argmax = 0.0;
+    for (int q = 0; q < 2; q++) {
+      while (!que[q].empty()) {
+        V top_v = que[q].top().second;
+        if (centered[top_v]) {
+          que[q].pop();
+          continue;
+        } else if (q == 0) {
+          if (!is_type1[top_v]) {
+            que[0].pop();
+            continue;
+          } else if (que[0].top().first > keys[top_v]) {
+            que[0].pop();
+            continue;
+          }
+        } else {
+          if (is_type1[top_v]) {
+            que[1].pop();
+            continue;
+          } else if (que[1].top().first > kp[top_v]) {
+            que[1].pop();
+            continue;
+          }
+        }
+        break;
+      }
+      if (que[q].empty()) continue;
+      V v = que[q].top().second;
+
+      set<V> tmp(Xs);
+      tmp.insert(X[v].begin(), X[v].end());
+      double ec_tmp = estimated_cardinality(g, tmp, k);
+      if (argmax < ec_tmp) {
+        argmax = ec_tmp;
+        selected_v = v;
+      }
+    }
+    if (selected_v < 0) {
+      break;
+    }
+    centers.push_back(selected_v);
+    centered[selected_v] = true;
+    {
+      if (!que[0].empty() && que[0].top().second == selected_v) que[0].pop();
+      if (!que[1].empty() && que[1].top().second == selected_v) que[1].pop();
+      multimap<V, V>::iterator itup = T.upper_bound(keys[selected_v]);
+      for (multimap<V, V>::iterator it = T.lower_bound(keys[selected_v]);
+           it != itup; it++)
+        if ((*it).second == selected_v) {
+          T.erase(it);
+          break;
+        }
+    }
+
+    // Merge
+    vector<V> delta;
+    for (V r : X[selected_v]) {
+      if (Xs.size() >= (size_t)k) {
+        V max_rank = *Xs.rbegin();
+        if (r > max_rank) break;
+        Xs.erase(max_rank);
+      }
+      Xs.insert(r);
+      delta.push_back(r);
+    }
+
+    // Update
+    reverse(delta.begin(), delta.end());
+    for (V ri : delta) {
+      for (pair<V, V> tp : T) {
+        if (tp.first >= ri) break;
+        V p = tp.second;
+        V i = inv[ri];
+        if (binary_search(X[p].begin(), X[p].end(), i)) continue;
+        if (is_type1[p]) {  // From type 1 to type 2
+          kp[p]--;
+          {
+            multimap<V, V>::iterator itup = T.upper_bound(keys[p]);
+            for (multimap<V, V>::iterator it = T.lower_bound(keys[p]);
+                 it != itup; it++)
+              if ((*it).second == p) {
+                T.erase(it);
+                break;
+              }
+          }
+          keys[p] = X[p][kp[p] - 1];
+          T.insert(make_pair(keys[p], p));
+          is_type1[p] = false;
+          que[1].push(make_pair(kp[p], p));
+        } else {  // From type 2 to type 1
+          is_type1[p] = true;
+          que[0].push(make_pair(keys[p], p));
+        }
+      }
+    }
+  }
+}
 
 // Select-Greedily O(n^2*k)
 void naive_select_greedily(const G &g, const vector<vector<V>> &X,
@@ -486,7 +605,6 @@ void naive_select_greedily(const G &g, const vector<vector<V>> &X,
   set<V> Xs;
   while (estimated_cardinality(g, Xs, k) < max(num_v, k - 1)) {
     V selected_v = -1;
-
     double argmax = 0.0;
     for (V v = 0; v < num_v; v++) {
       if (centered[v]) continue;
@@ -546,7 +664,8 @@ vector<V> box_cover_sketch(const G &g, W radius, const int k,
     //
     // Select-Greedily O(n^2*k)
     //
-    naive_select_greedily(g, X, centers, centered, k);
+    // naive_select_greedily(g, X, centers, centered, k);
+    select_greedily(g, X, inv, centers, centered, k);
   }
 
   return centers;
