7c4eb5e009a457bed6e1a29af791f27b1f48e17d
kenkoooo <kenkou.n@gmail.com>
2016-01-06 19:45:30 +0900

    解析解に合わせた頂点のみ計算できるようにした

--- playground/box_cover_main.cc
+++ playground/box_cover_main.cc
@@ -2,12 +2,61 @@
 #include "box_cover.h"
 using namespace std;
 
-DEFINE_int32(rad_min, 0, "minimum radius");
+DEFINE_int32(rad_min, 1, "minimum radius");
 DEFINE_int32(rad_max, 10, "maximum radius");
 DEFINE_string(method, "sketch", "using method");
 DEFINE_double(final_coverage, 0.98, "coverage");
 DEFINE_int32(pass, 1, "Number of multi-pass");
 DEFINE_int32(sketch_k, 1024, "sketch k");
+DEFINE_bool(rad_analytical, false, "Using analytical diameters as rads");
+
+unweighted_edge_list extract_maximal_connected(const G& g_pre) {
+  unweighted_edge_list es;
+  V num_v = g_pre.num_vertices();
+  size_t max_num_v = 0;
+  int max_g = 0;
+  vector<bool> vis(num_v, false);
+  vector<vector<V>> connected_sets;
+  for (V v = 0; v < num_v; ++v) {
+    if (vis[v]) continue;
+    vector<V> connected_v;
+    queue<V> que;
+    que.push(v);
+    vis[v] = true;
+    connected_v.push_back(v);
+    while (!que.empty()) {
+      V u = que.front();
+      que.pop();
+      for (V x : g_pre.neighbors(u)) {
+        if (!vis[x]) {
+          vis[x] = true;
+          que.push(x);
+          connected_v.push_back(x);
+        }
+      }
+    }
+    if (max_num_v < connected_v.size()) {
+      max_num_v = connected_v.size();
+      max_g = connected_sets.size();
+      sort(connected_v.begin(), connected_v.end());
+      connected_sets.push_back(connected_v);
+    }
+  }
+
+  vector<V>& max_c = connected_sets[max_g];
+  vector<V> inv(num_v, -1);
+  for (V v : max_c) {
+    inv[v] = (lower_bound(max_c.begin(), max_c.end(), v) - max_c.begin());
+  }
+
+  for (pair<V, V> e : g_pre.edge_list()) {
+    V from = e.first;
+    V to = e.second;
+    if (inv[from] == -1) continue;
+    es.emplace_back(inv[from], inv[to]);
+  }
+  return es;
+}
 
 int main(int argc, char** argv) {
   auto str_replace = [](string& str, const string& from, const string& to) {
@@ -18,58 +67,10 @@ int main(int argc, char** argv) {
     }
   };
 
-  //
   // Extract maximal connected subgraph & Compress coordinates
-  //
-  unweighted_edge_list es;
-  {
-    G g_pre = easy_cui_init(argc, argv);
-    CHECK_MSG(FLAGS_force_undirected, "undirected only!!!");
-
-    V num_v = g_pre.num_vertices();
-    size_t max_num_v = 0;
-    int max_g = 0;
-    vector<bool> vis(num_v, false);
-    vector<vector<V>> connected_sets;
-    for (V v = 0; v < num_v; ++v) {
-      if (vis[v]) continue;
-      vector<V> connected_v;
-      queue<V> que;
-      que.push(v);
-      vis[v] = true;
-      connected_v.push_back(v);
-      while (!que.empty()) {
-        V u = que.front();
-        que.pop();
-        for (V x : g_pre.neighbors(u)) {
-          if (!vis[x]) {
-            vis[x] = true;
-            que.push(x);
-            connected_v.push_back(x);
-          }
-        }
-      }
-      if (max_num_v < connected_v.size()) {
-        max_num_v = connected_v.size();
-        max_g = connected_sets.size();
-        sort(connected_v.begin(), connected_v.end());
-        connected_sets.push_back(connected_v);
-      }
-    }
-
-    vector<V>& max_c = connected_sets[max_g];
-    vector<V> inv(num_v, -1);
-    for (V v : max_c) {
-      inv[v] = (lower_bound(max_c.begin(), max_c.end(), v) - max_c.begin());
-    }
-
-    for (pair<V, V> e : g_pre.edge_list()) {
-      V from = e.first;
-      V to = e.second;
-      if (inv[from] == -1) continue;
-      es.emplace_back(inv[from], inv[to]);
-    }
-  }
+  G g_pre = easy_cui_init(argc, argv);
+  CHECK_MSG(FLAGS_force_undirected, "undirected only!!!");
+  unweighted_edge_list es = extract_maximal_connected(g_pre);
 
   G g(es);
   pretty_print(g);
@@ -79,36 +80,61 @@ int main(int argc, char** argv) {
     JLOG_PUT("graph", FLAGS_graph);
   }
 
+  // Extract Graph Name
   const char* slash = strrchr(FLAGS_graph.c_str(), '/');
   string graph_name = slash ? slash + 1 : FLAGS_graph;
   str_replace(graph_name, " ", "-");
 
+  vector<W> rads;
+  if (FLAGS_rad_analytical) {
+    istringstream iss(FLAGS_graph);
+    string family;
+    iss >> family;
+    V required, u, v;
+    if (!(iss >> required)) required = 44;
+    if (!(iss >> u)) u = 2;
+    if (!(iss >> v)) v = 2;
+    auto as = find_analytical_solution(family, u, v, g);
+    for (auto p : as) {
+      W diameter = p.first;
+      W rad = (diameter + 1) / 2;
+      if (rad >= FLAGS_rad_max) break;
+      rads.push_back(rad);
+    }
+  } else {
+    for (W rad = FLAGS_rad_min; rad <= FLAGS_rad_max; ++rad) {
+      rads.push_back(rad);
+    }
+  }
+
   string experiment_name = FLAGS_method;
   if (FLAGS_method == "sketch") {
+    if (FLAGS_pass == 1) {
+      FLAGS_final_coverage = 1.0;
+    }
     experiment_name.append("-k." + to_string(FLAGS_sketch_k));
     experiment_name.append("-pass." + to_string(FLAGS_pass));
     experiment_name.append("-coverage." + to_string(FLAGS_final_coverage));
 
-    if (FLAGS_pass == 1) {
-      FLAGS_final_coverage = 1.0;
-    }
     JLOG_PUT("name", experiment_name);
+    string sk = to_string(FLAGS_sketch_k);
+    while (sk.size() < 4) sk = "0" + sk;
     JLOG_PUT("k", to_string(FLAGS_sketch_k));
     JLOG_PUT("pass", to_string(FLAGS_pass));
 
-    for (W rad = FLAGS_rad_min; rad <= FLAGS_rad_max; ++rad) {
+    for (W rad : rads) {
       vector<V> res;
+      double coverage_bechmark = FLAGS_final_coverage;
       JLOG_ADD_BENCHMARK("time") res = box_cover_sketch(
-          g, rad, FLAGS_sketch_k, FLAGS_pass, FLAGS_final_coverage);
+          g, rad, FLAGS_sketch_k, FLAGS_pass, coverage_bechmark);
       JLOG_ADD("size", res.size());
       JLOG_ADD("radius", rad);
-      JLOG_ADD("coverage", coverage(g, res, rad));
+      JLOG_ADD("coverage", coverage_bechmark);
       if (res.size() == 1) break;
     }
   } else if (FLAGS_method == "memb") {
     JLOG_PUT("name", "MEMB");
-
-    for (W rad = FLAGS_rad_min; rad <= FLAGS_rad_max; ++rad) {
+    for (W rad : rads) {
       vector<V> res;
       JLOG_ADD_BENCHMARK("time") res = box_cover_memb(g, rad);
       JLOG_ADD("size", res.size());
@@ -116,7 +142,6 @@ int main(int argc, char** argv) {
       JLOG_ADD("coverage", coverage(g, res, rad));
       if (res.size() == 1) break;
     }
-
   } else if (FLAGS_method == "analytical") {
     istringstream iss(FLAGS_graph);
     string family;
@@ -135,8 +160,7 @@ int main(int argc, char** argv) {
     }
   } else if (FLAGS_method == "burning") {
     JLOG_PUT("name", "Burning");
-
-    for (W rad = FLAGS_rad_min; rad <= FLAGS_rad_max; ++rad) {
+    for (W rad : rads) {
       vector<V> res;
       JLOG_ADD_BENCHMARK("time") res = box_cover_burning(g, rad);
       JLOG_ADD("size", res.size());
@@ -144,7 +168,6 @@ int main(int argc, char** argv) {
       JLOG_ADD("coverage", coverage(g, res, rad));
       if (res.size() == 1) break;
     }
-
   } else {
     cerr << "Unknown method: " << FLAGS_method << endl;
     return 0;
