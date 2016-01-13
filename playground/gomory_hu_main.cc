da60f290d8ebfee3c481a54cbd11dbb835c71504
math <syousuke@nii.ac.jp>
2016-01-13 20:22:36 +0900

    cutが2になるものも枝狩りした

--- playground/gomory_hu_main.cc
+++ playground/gomory_hu_main.cc
@@ -1,6 +1,10 @@
 #include <easy_cui.h>
 
 #ifdef _WIN32
+#pragma comment(linker, "/STACK:3200000") 
+#endif
+
+#ifdef _WIN32
 template<typename GraphType = G>
 GraphType easy_cui_init(int argc, char **argv) {
   JLOG_INIT(&argc, argv);
@@ -567,6 +571,107 @@ private:
   vector<int> depth_;
 };
 
+class OptimizedGusfield2 {
+  void build_depth() {
+    depth_[root_node_] = 0;
+
+    stack<int> stk;
+    FOR(v, num_vertices_) {
+      while (depth_[v] == -1) {
+        stk.push(v);
+        v = parent_weight_[v].first;
+      }
+      while (!stk.empty()) {
+        int u = stk.top(); stk.pop();
+        depth_[u] = depth_[v] + 1;
+        v = u;
+      }
+    }
+  }
+
+public:
+
+  OptimizedGusfield2(vector<pair<V, V>>& edges, int num_vs) :
+    num_vertices_(num_vs),
+    parent_weight_(num_vs, make_pair(-1, 0)),
+    depth_(num_vs, -1) {
+
+    vector<int> degree(num_vertices_);
+    for (auto& e : edges) degree[e.first]++, degree[e.second]++;
+
+    root_node_ = -1;
+    FOR(v, num_vertices_) if (degree[v] != 2) {
+      root_node_ = v; break;
+    }
+    if (root_node_ == -1) root_node_ = 0;
+
+    vector<int> mincut_order;
+    FOR(v, num_vertices_) {
+      if (v == root_node_) continue;
+      parent_weight_[v].first = root_node_;
+      if (degree[v] == 2) {
+        parent_weight_[v].second = 2;
+      } else {
+        mincut_order.push_back(v);
+      }
+    }
+    parent_weight_[root_node_].first = -1;
+
+    dinic_twosided dc_base(edges, num_vs);
+
+    vector<int> used(num_vertices_);
+    for (V s : mincut_order) {
+      V t = parent_weight_[s].first;
+
+      auto dc = dc_base;
+      int cost = dc.max_flow(s, t);
+      fprintf(stderr, "(%d,%d) cost = %d\n", s, t, cost);
+      parent_weight_[s].second = cost;
+
+      if (degree[s] == cost) continue;
+
+      queue<int> q;
+      q.push(s);
+      const int F = s + 1;
+      used[s] = F;
+      while (!q.empty()) {
+        V v = q.front(); q.pop();
+        for (auto& e : dc.e[v]) {
+          if (e.cap(dc.graph_revision) == 0 || used[e.to] == F) continue;
+          used[e.to] = F;
+          q.push(e.to);
+          if (parent_weight_[e.to].first == t) parent_weight_[e.to].first = s;
+        }
+      }
+    }
+
+    build_depth();
+  }
+
+  int query(V u, V v) {
+    CHECK(u != v);
+    CHECK(u < num_vertices_ && v < num_vertices_);
+    int ans = numeric_limits<int>::max();
+    while (u != v) {
+      if (depth_[u] > depth_[v]) {
+        ans = min(ans, parent_weight_[u].second);
+        u = parent_weight_[u].first;
+      } else {
+        ans = min(ans, parent_weight_[v].second);
+        v = parent_weight_[v].first;
+      }
+    }
+    return ans;
+  }
+
+private:
+  const int num_vertices_;
+  vector<pair<V, int>> parent_weight_;
+  vector<int> depth_;
+
+  int root_node_;
+};
+
 template<class handler_t>
 class ConnectedComponentsFilter {
 public:
@@ -616,6 +721,10 @@ public:
     if (!uf.is_same(u, v)) return 0;
     int lu = local_indices[u], lv = local_indices[v];
     auto& handler = handlers[handlers_indices[uf.root(u)]];
+    if (lu == lv) {
+      printf("u = %d, lu = %d, v = %d, lv = %d\n", u, lu, v, lv);
+      CHECK(false);
+    }
     return handler.query(lu, lv);
   }
 
@@ -627,35 +736,34 @@ private:
 };
 
 
-class OptimizedGusfield2 {
+// 2ECC = 2-edge connected components
+class OptimizedGusfieldWith2ECC {
 public:
   const int n;
+  const G& g;
 
-  OptimizedGusfield2(const G& g) : n(g.num_vertices()), uf(n) {
+  void lowlink_dfs(int v, int par, int& cur_ord) {
+    lowlink[v] = order[v] = cur_ord++;
+    FOR(dir, 2) for (auto to : g.edges(v, D(dir))) {
+      if (to == par) continue;
+      if (order[to] == -1) {
+        lowlink_dfs(to, v, cur_ord);
+        lowlink[v] = min(lowlink[v], lowlink[to]);
+        if (order[v] < lowlink[to]) bridge.emplace_back(v, to);
+        else biconnected_graphs_edges.emplace_back(v, to);
+      } else {
+        lowlink[v] = min(lowlink[v], lowlink[to]);
+        if (v < to) biconnected_graphs_edges.emplace_back(v, to);
+      }
+    }
+  }
+
+  OptimizedGusfieldWith2ECC(const G& g) : n(g.num_vertices()), g(g), uf(n), lowlink(n, -1), order(n, -1) {
     FOR(v, n) for (auto& e : g.edges(v)) {
       uf.unite(v, to(e));
     }
 
     const int num_edges = g.num_edges();
-
-    vector<int> lowlink(n, -1), order(n, -1);
-    vector<pair<V, V>> bridge, biconnected_graphs_edges;
-
-    function<void(int, int, int&)> lowlink_dfs = [&](int v, int par, int& cur_ord) {
-      lowlink[v] = order[v] = cur_ord++;
-      FOR(dir, 2) for (auto to : g.edges(v, D(dir))) {
-        if (to == par) continue;
-        if (order[to] == -1) {
-          lowlink_dfs(to, v, cur_ord);
-          lowlink[v] = min(lowlink[v], lowlink[to]);
-          if (order[v] < lowlink[to]) bridge.emplace_back(v, to);
-          else biconnected_graphs_edges.emplace_back(v, to);
-        } else {
-          lowlink[v] = min(lowlink[v], lowlink[to]);
-          if (v < to) biconnected_graphs_edges.emplace_back(v, to);
-        }
-      }
-    };
 
     FOR(v, n) if (uf.root(v) == v) {
       int cur_ord = 0;
@@ -665,7 +773,7 @@ public:
     CHECK(sz(bridge) + sz(biconnected_graphs_edges) == num_edges);
 
     G new_g(biconnected_graphs_edges, n);
-    biconnected_graph_handler.reset(new ConnectedComponentsFilter<OptimizedGusfield>(new_g));
+    biconnected_graph_handler.reset(new ConnectedComponentsFilter<OptimizedGusfield2>(new_g));
   }
 
 public:
@@ -680,7 +788,10 @@ public:
 
 private:
   union_find uf;
-  unique_ptr<ConnectedComponentsFilter<OptimizedGusfield>> biconnected_graph_handler;
+  vector<int> lowlink, order;
+  vector<pair<V, V>> bridge, biconnected_graphs_edges;
+
+  unique_ptr<ConnectedComponentsFilter<OptimizedGusfield2>> biconnected_graph_handler;
 };
 
 G to_directed_graph(G g) {
@@ -692,7 +803,7 @@ G to_directed_graph(G g) {
 }
 
 using Gusfield2 = ConnectedComponentsFilter<OptimizedGusfield>;
-using Gusfield3 = OptimizedGusfield2;
+using Gusfield3 = OptimizedGusfieldWith2ECC;
 
 bool check_min_cut_query(Gusfield2& gf2, Gusfield3& gf3, int s, int t, const G& g) {
   // int naive_w = dc.max_flow(s, t);
@@ -710,30 +821,47 @@ void test(const G& g) {
   Gusfield2 gf2(g);
   xorshift64star gen_node(FLAGS_node_pair_random_seed);
 
-  int unmatch = 0;
-  for (int counter = 0; counter < FLAGS_num_query; counter++) {
-    V s = gen_node() % g.num_vertices();
-    V t = gen_node() % (g.num_vertices() - 1);
-    if (s <= t) t++;
-    if (counter % 100 == 0) {
-      fprintf(stderr, "count/unmatch/all : %d/%d/%d, \n", counter, unmatch, FLAGS_num_query);
+  if (g.num_vertices() > 5000) {
+    int unmatch = 0;
+    for (int counter = 0; counter < FLAGS_num_query; counter++) {
+      V s = gen_node() % g.num_vertices();
+      V t = gen_node() % (g.num_vertices() - 1);
+      if (s <= t) t++;
+      if (counter % 100 == 0) {
+        fprintf(stderr, "count/unmatch/all : %d/%d/%d, \n", counter, unmatch, FLAGS_num_query);
+      }
+      bool is_matched = check_min_cut_query(gf2, gf3, s, t, g);
+      if (!is_matched) unmatch++;
     }
-    bool is_matched = check_min_cut_query(gf2, gf3, s, t, g);
-    if (!is_matched) unmatch++;
+    JLOG_PUT("result.all", FLAGS_num_query);
+    JLOG_PUT("result.match", (FLAGS_num_query - unmatch));
+    JLOG_PUT("result.unmatch", unmatch);
+  } else {
+    int counter = 0;
+    int unmatch = 0;
+    FOR(s, g.num_vertices()) for (int t = s + 1; t < g.num_vertices(); t++) {
+      if (counter % 100 == 0) {
+        fprintf(stderr, "count/unmatch/all : %d/%d/%d, \n", counter, unmatch, -1);
+      }
+      bool is_matched = check_min_cut_query(gf2, gf3, s, t, g);
+      if (!is_matched) unmatch++;
+      counter++;
+    }
+
+    JLOG_PUT("result.all", counter);
+    JLOG_PUT("result.match", (counter - unmatch));
+    JLOG_PUT("result.unmatch", unmatch);
   }
-  JLOG_PUT("result.all", FLAGS_num_query);
-  JLOG_PUT("result.match", (FLAGS_num_query - unmatch));
-  JLOG_PUT("result.unmatch", unmatch);
 }
 
 void tester() {
-  test(to_directed_graph(built_in_graph("karate_club")));
-  test(to_directed_graph(built_in_graph("dolphin")));
   test(to_directed_graph(built_in_graph("ca_grqc")));
   exit(0);
 }
 
 int main(int argc, char** argv) {
+  // tester();
+
   G g = easy_cui_init(argc, argv);
   g = to_directed_graph(g);
 
