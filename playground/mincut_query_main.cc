b2f3946bfb0ee1777d5606ce7381cc68a6ec6aee
math <syousuke@nii.ac.jp>
2016-02-10 18:42:10 +0900

    縮約後の仮頂点を取り除く処理を加えて、リファクタリングした

--- playground/mincut_query_main.cc
+++ playground/mincut_query_main.cc
@@ -3,59 +3,303 @@
 #define FOR(i,n) for(int i = 0; i < (n); i++)
 #define sz(c) ((int)(c).size())
 
-#include "dinic_twosided.h"
 #include "dinic_naive.h"
+#include "dinic_twosided.h"
 #include "random_contraction.h"
 
 DEFINE_int32(num_query, 1000, "");
 DEFINE_int64(node_pair_random_seed, 922337203685477583LL, "");
-DEFINE_string(method,"test","test, single_source_mincut");
+DEFINE_string(method, "test", "test, single_source_mincut");
 
-bool check_min_cut_query(const min_cut_query& mcq,int s,int t,G& g){
-  dinic_naive nv(g);
-  int naive_w = nv.max_flow(s,t);
-  int mcq_w = mcq.query(s,t);
+bool check_min_cut_query(const min_cut_query& mcq, dinic_twosided& dc, int s, int t, G& g) {
+  dc.reset_graph();
+  int naive_w = dc.max_flow(s, t);
+  int mcq_w = mcq.query(s, t);
+  if (naive_w > mcq_w) {
+    dinic_naive dn(g);
+    int val = dn.max_flow(s, t);
+    printf("naive = %d, mcq = %d, dinic_twosided = %d\n",val,mcq_w, naive_w);
+  }
   CHECK(naive_w <= mcq_w);
 
   JLOG_ADD_OPEN("query") {
-    JLOG_PUT("S",s);
-    JLOG_PUT("T",t);
+    JLOG_PUT("S", s);
+    JLOG_PUT("T", t);
     JLOG_PUT("naive", naive_w);
     JLOG_PUT("mcq_w", mcq_w);
   }
-  if(naive_w != mcq_w) {
-    fprintf(stderr, "unmatched. (S,T) = (%d,%d), naive = %d, mcq_w =%d\n",s,t,naive_w,mcq_w);
+  if (naive_w != mcq_w) {
+    fprintf(stderr, "unmatched. (S,T) = (%d,%d), naive = %d, mcq_w =%d\n", s, t, naive_w, mcq_w);
   }
   return naive_w == mcq_w;
 }
 
+class random_contraction2 {
+  struct degree_weight {
+    int degree, weight;
+    degree_weight() : degree(0), weight(0) {}
+    degree_weight(int degree, int weight) : degree(degree), weight(weight) {}
+    bool operator<(const degree_weight& r) const {
+      return degree < r.degree;
+    }
+    degree_weight& operator+=(const degree_weight& r) {
+      degree += r.degree;
+      weight += r.weight;
+      return *this;
+    }
+    degree_weight operator+(const degree_weight& r) const {
+      degree_weight ret = *this;
+      return ret += r;
+    }
+  };
+  void build_depth() {
+    depth_.resize(sz(binary_tree_edges_), -1);
+    parent_weight_.resize(sz(binary_tree_edges_), make_pair(-2, -2));
+
+    FOR(v, num_vertices_) {
+      if (depth_[v] >= 0) continue;
+
+      depth_[v] = 0;
+      parent_weight_[v] = make_pair(-1, -1);
+      queue<int> q;
+      q.push(v);
+      while (!q.empty()) {
+        int u = q.front(); q.pop();
+        for (auto& to_weight : binary_tree_edges_[u]) {
+          int to, weight; tie(to, weight) = to_weight;
+          if (depth_[to] >= 0) continue;
+          depth_[to] = depth_[u] + 1;
+          parent_weight_[to].first = u;
+          parent_weight_[to].second = weight;
+          // printf("%d - %d\n",u, to);
+          q.push(to);
+        }
+      }
+    }
+  }
+
+  void contraction(vector<int>& ancestor, vector<unordered_map<V, int>>& contraction_graph_edges, V u, V v, queue<int>& degree_eq_1) {
+    assert(!uf_.is_same(u, v));
+    V new_vertex = (V)binary_tree_edges_.size();
+    u = uf_.root(u), v = uf_.root(v);
+    degree_weight uw = uf_.weight(u), vw = uf_.weight(v);
+    uf_.unite(u, v);
+    if (u != uf_.root(u)) {
+      swap(u, v);
+      swap(uw, vw);
+    }
+
+    int ua = ancestor[u], va = ancestor[v];
+    ancestor[u] = new_vertex;
+
+    // 縮約した結果を元にbinary treeの構築を進める
+    binary_tree_edges_.emplace_back(); // new_vertex分の確保
+    binary_tree_edges_[new_vertex].emplace_back(ua, uw.weight);
+    binary_tree_edges_[ua].emplace_back(new_vertex, uw.weight);
+    binary_tree_edges_[new_vertex].emplace_back(va, vw.weight);
+    binary_tree_edges_[va].emplace_back(new_vertex, vw.weight);
+
+    //縮約した頂点の辺をまとめる
+    auto& uset = contraction_graph_edges[u];
+    auto& vset = contraction_graph_edges[v];
+    int dec_weight = 0;
+    for (const auto& edge : vset) {
+      V to; int w; tie(to, w) = edge;
+      if (to == u) {
+        auto it = uset.find(v);
+        assert(it != uset.end());
+        assert(it->second == w);
+        uset.erase(it);
+        dec_weight += w * 2;
+      } else {
+        uset[to] += w;
+        auto& toset = contraction_graph_edges[to];
+        auto it = toset.find(v);
+        toset.erase(it);
+        toset[u] += w;
+        if (FLAGS_prune_if_degree_eq_1 && sz(toset) == 1) {
+          degree_eq_1.push(to);
+        }
+      }
+    }
+    vset.clear();
+    uf_.add_weight(u, degree_weight(-2, -dec_weight));
+  }
+
+public:
+  // g の辺を使い、グラフを作成する(勝手にundirectedとして読み替えている)
+  random_contraction2(G& g) :
+    num_vertices_(g.num_vertices()),
+    uf_(g.num_vertices()),
+    binary_tree_edges_(g.num_vertices()) {
+    //unordered -> weight = 1なので、shuffleでよい
+    //重みがあるならBITで。(stochastic acceptanceはupdateありだと使えない)
+    vector<unordered_map<V, int>> contraction_graph_edges(g.num_vertices());
+    typename G::edge_list_type initial_edges(g.edge_list());
+    std::shuffle(initial_edges.begin(), initial_edges.end(), agl::random);
+    vector<int> ancestor(g.num_vertices());
+
+    FOR(i, num_vertices_) ancestor[i] = i;
+
+    for (auto edge : initial_edges) {
+      V u = edge.first, v = to(edge.second);
+      contraction_graph_edges[u].emplace(v, 1);
+      contraction_graph_edges[v].emplace(u, 1);
+    }
+    FOR(v, num_vertices_) uf_.add_weight(v, degree_weight(sz(contraction_graph_edges[v]), sz(contraction_graph_edges[v])));
+
+    queue<int> degree_eq_1;
+    int pruned_degree_eq_1 = 0;
+    if (FLAGS_prune_if_degree_eq_1) {
+      FOR(v, num_vertices_) {
+        if (sz(contraction_graph_edges[v]) == 1) degree_eq_1.push(v);
+      }
+    }
+
+    for (const auto& edge : initial_edges) {
+      if (FLAGS_prune_if_degree_eq_1) {
+        // 次数1の頂点があればそれを縮約する
+        while (!degree_eq_1.empty()) {
+          V v = degree_eq_1.front(); degree_eq_1.pop();
+          if (contraction_graph_edges[v].size() == 0) continue;
+          V u;
+          tie(u, std::ignore) = *contraction_graph_edges[v].begin();
+          contraction(ancestor, contraction_graph_edges, u, v, degree_eq_1);
+          if (contraction_graph_edges[u].size() == 1) degree_eq_1.push(u);
+
+          pruned_degree_eq_1++;
+        }
+      }
+
+      V u = edge.first, v = to(edge.second);
+      if (uf_.is_same(u, v)) continue;
+      contraction(ancestor, contraction_graph_edges, u, v, degree_eq_1);
+    }
+    // gが連結とは限らないので、uv_costs.size() == g.num_vertices() - 1ではない
+    assert(int(binary_tree_edges_.size() - g.num_vertices()) <= g.num_vertices() - 1);
+
+    build_depth();
+  }
+
+  int query(V u, V v) const {
+    CHECK(u != v);
+    CHECK(u < num_vertices_ && v < num_vertices_);
+    int ans = numeric_limits<int>::max();
+    while (u != v) {
+      if (u == -1 || v == -1) return 0; // disconnect
+      if (depth_[u] > depth_[v]) swap(u, v);
+      ans = min(ans, parent_weight_[v].second);
+      v = parent_weight_[v].first;
+    }
+    return ans;
+  }
+
+  void single_source_mincut_dfs(V v, V par, int mn, vector<int>& ans) const {
+    if (v < num_vertices_) {
+      ans[v] = mn;
+    }
+    for (auto& to : binary_tree_edges_[v]) {
+      if (to.first == par) continue;
+      int nmn = min(mn, to.second);
+      single_source_mincut_dfs(to.first, v, nmn, ans);
+    }
+  }
+
+
+  vector<int> single_source_mincut(V u) const {
+    vector<int> ans(num_vertices_, numeric_limits<int>::max());
+    single_source_mincut_dfs(u, -1, numeric_limits<int>::max(), ans);
+    return ans;
+  }
+
+private:
+  const int num_vertices_;
+  weighted_union_find<degree_weight> uf_;
+
+  vector<vector<pair<V, int>>> binary_tree_edges_;
+  vector<pair<V, int>> parent_weight_;
+  vector<int> depth_;
+};
+
+class min_cut_query2 {
+public:
+  min_cut_query2(G& g) {
+    fprintf(stderr, "initialize mincut-query tree ...\n");
+    for (int i = 0; i < FLAGS_solver_iter; i++) {
+      if (i % 10 == 0) fprintf(stderr, "trees .. %d/%d\n", i, FLAGS_solver_iter);
+      solvers_.emplace_back(g);
+    }
+    fprintf(stderr, "completed.\n");
+  }
+
+  int query(int u, int v) const {
+    int ans = numeric_limits<int>::max();
+    for (const auto& solver : solvers_) {
+      ans = min(ans, solver.query(u, v));
+    }
+    return ans;
+  }
+
+  vector<int> single_source_mincut(V v) const {
+    vector<int> ret;
+    for (const auto& solver : solvers_) {
+      auto cur_ans = solver.single_source_mincut(v);
+      if (sz(ret) == 0) ret = move(cur_ans);
+      else {
+        FOR(i, sz(ret)) ret[i] = min(ret[i], cur_ans[i]);
+      }
+    }
+    return ret;
+  }
+private:
+  vector<random_contraction2> solvers_;
+};
+
 void test(G&& g) {
-    min_cut_query mcq(g);
+  dinic_twosided dc(g);
+  min_cut_query mcq(g);
   // mcq.debug_output_graph();
 
   JLOG_PUT("argv.mincut_tree_iter", FLAGS_solver_iter);
 
   xorshift64star gen_node(FLAGS_node_pair_random_seed);
   int unmatch = 0;
-  for(int counter = 0; counter < FLAGS_num_query; counter++) {
+  for (int counter = 0; counter < FLAGS_num_query; counter++) {
     V s = gen_node() % g.num_vertices();
     V t = gen_node() % (g.num_vertices() - 1);
-    if(s <= t) t++;
-    if(counter % 100 == 0){
-      fprintf(stderr, "count/unmatch/all : %d/%d/%d, \n",counter, unmatch, FLAGS_num_query);
+    if (s <= t) t++;
+    if (counter % 100 == 0) {
+      fprintf(stderr, "count/unmatch/all : %d/%d/%d, \n", counter, unmatch, FLAGS_num_query);
     }
-    bool is_matched = check_min_cut_query(mcq, s, t, g);
-    if(!is_matched) unmatch++;
+    bool is_matched = check_min_cut_query(mcq, dc, s, t, g);
+    if (!is_matched) unmatch++;
   }
   JLOG_PUT("result.all", FLAGS_num_query);
   JLOG_PUT("result.match", (FLAGS_num_query - unmatch));
   JLOG_PUT("result.unmatch", unmatch);
 }
 
-G to_directed_graph(G& g){
-  vector<pair<V,V>> ret;
-  for(auto& e : g.edge_list()) {
-      if(e.first < to(e.second)) ret.emplace_back(e.first,to(e.second));
+void test2(G&& g) {
+  auto r = agl::random;
+  agl::random = r;
+  min_cut_query q1(g);
+  agl::random = r;
+  min_cut_query2 q2(g);
+
+  const int n = g.num_vertices();
+  FOR(v, n) for (int s = v + 1; s < n; s++) {
+    auto ans1 = q1.query(v, s);
+    auto ans2 = q2.query(v, s);
+    if (ans1 != ans2) {
+      cout << "?";
+    }
+  }
+}
+
+G to_directed_graph(G&& g) {
+  vector<pair<V, V>> ret;
+  for (auto& e : g.edge_list()) {
+    if (e.first < to(e.second)) ret.emplace_back(e.first, to(e.second));
   }
   return G(ret);
 }
@@ -63,73 +307,79 @@ G to_directed_graph(G& g){
 string graph_name() {
   string x = FLAGS_graph;
   string ret;
-  for(int i = sz(x) - 1; i >= 0; i--) {
-   if(x[i] == '/' || x[i] == '\\') break;
+  for (int i = sz(x) - 1; i >= 0; i--) {
+    if (x[i] == '/' || x[i] == '\\') break;
     ret.push_back(x[i]);
   }
-  reverse(ret.begin(),ret.end());
+  reverse(ret.begin(), ret.end());
   return ret;
 }
 
 DEFINE_string(single_source_mincut_output, "random_contraction_ssm.data", "");
 void single_source_mincut(G&& g) {
-    min_cut_query mcq(g);
-    size_t max_deg = 0;
-    int max_deg_v = -1;
-    FOR(v, g.num_vertices()) {
-      if(g.degree(v, D(0)) + g.degree(D(1)) > max_deg) {
-        max_deg = g.degree(v, D(0)) + g.degree(D(1));
-        max_deg_v = v;
-      }
+  min_cut_query mcq(g);
+  size_t max_deg = 0;
+  int max_deg_v = -1;
+  FOR(v, g.num_vertices()) {
+    if (g.degree(v, D(0)) + g.degree(D(1)) > max_deg) {
+      max_deg = g.degree(v, D(0)) + g.degree(D(1));
+      max_deg_v = v;
     }
+  }
 
-    auto anses = mcq.single_source_mincut(max_deg_v);
-    FILE* fp = fopen(FLAGS_single_source_mincut_output.c_str(), "w");
-    FOR(v,g.num_vertices()) {
-      if(v == max_deg_v) continue;
-      
-      fprintf(fp, "%d %d %d\n",max_deg_v, v, anses[v]);
-    }  
-    fclose(fp);
+  FILE* fp = fopen(FLAGS_single_source_mincut_output.c_str(), "w");
+  FOR(v, g.num_vertices()) {
+    if (v == max_deg_v) continue;
+    int xx = mcq.query(max_deg_v, v);
+    fprintf(fp, "%d %d %d\n", max_deg_v, v, xx);
+  }
+  fclose(fp);
 }
 
 void single_source_mincut_with_deg(G&& g) {
-    size_t max_deg = 0;
-    int max_deg_v = -1;
-    FOR(v, g.num_vertices()) {
-      if(g.degree(v) > max_deg) {
-        max_deg = g.degree(v);
-        max_deg_v = v;
-      }
+  size_t max_deg = 0;
+  int max_deg_v = -1;
+  FOR(v, g.num_vertices()) {
+    if (g.degree(v) > max_deg) {
+      max_deg = g.degree(v);
+      max_deg_v = v;
     }
+  }
 
-    FILE* fp = fopen(FLAGS_single_source_mincut_output.c_str(), "w");
-    FOR(v,g.num_vertices()) {
-      if(v == max_deg_v) continue;
-      int xx = g.degree(v, D(0)) + g.degree(v, D(1));
-      fprintf(fp, "%d %d %d\n",max_deg_v, v, xx);
-    }  
-    fclose(fp);
+  FILE* fp = fopen(FLAGS_single_source_mincut_output.c_str(), "w");
+  FOR(v, g.num_vertices()) {
+    if (v == max_deg_v) continue;
+    int xx = g.degree(v, D(0)) + g.degree(v, D(1));
+    fprintf(fp, "%d %d %d\n", max_deg_v, v, xx);
+  }
+  fclose(fp);
 }
 
+void tester() {
+  test(to_directed_graph(built_in_graph("karate_club")));
+  test(to_directed_graph(built_in_graph("dolphin")));
+  test(to_directed_graph(built_in_graph("ca_grqc")));
+
+  return;
+}
 
 int main(int argc, char **argv) {
   // JLOG_INIT(&argc, argv); called in "easy_cui_init"
+  // tester();
   G g = easy_cui_init(argc, argv);
-  g = to_directed_graph(g);
+  g = to_directed_graph(std::move(g));
 
-  if(FLAGS_method == "test") {
+  if (FLAGS_method == "test") {
     test(move(g));
-  } else if(FLAGS_method == "single_source_mincut") {
+  } else if (FLAGS_method == "single_source_mincut") {
     single_source_mincut(move(g));
-  } else if(FLAGS_method == "single_source_mincut_with_deg") {
+  } else if (FLAGS_method == "single_source_mincut_with_deg") {
     single_source_mincut_with_deg(move(g));
   } else {
-      fprintf(stderr, "unrecognized option '-method=%s'\n", FLAGS_method.c_str());
-      exit(-1);
+    fprintf(stderr, "unrecognized option '-method=%s'\n", FLAGS_method.c_str());
+    exit(-1);
   }
 
-  
+
   return 0;
 }
- 
--- playground/random_contraction.h
+++ playground/random_contraction.h
@@ -1,29 +1,11 @@
 #pragma once
+#include "ConnectedComponentsFilter.h"
 
 DEFINE_bool(prune_if_degree_eq_1, true, "");
 DEFINE_int32(solver_iter, 50, "");
 
-struct degree_weight {
-  int degree, weight;
-  degree_weight() : degree(0), weight(0) {}
-  degree_weight(int degree, int weight) : degree(degree), weight(weight) {}
-  bool operator<(const degree_weight& r) const {
-    return degree < r.degree;
-  }
-  degree_weight& operator+=(const degree_weight& r) {
-    degree += r.degree;
-    weight += r.weight;
-    return *this;
-  }
-  degree_weight operator+(const degree_weight& r) const {
-    degree_weight ret = *this;
-    return ret += r;
-  }
-};
-
-
+template<class weight_t>
 class weighted_union_find {
-  using weight_t = degree_weight;
 public:
   weighted_union_find(int n) : n_(n), uf_(n_), par_(n_), weight_(n_) {
     FOR(i, n_) par_[i] = i;
@@ -61,13 +43,19 @@ private:
   vector<weight_t> weight_;
 };
 
-class random_contraction {
+class tree_minimum_query {
+  void build(const vector<tuple<V, V, int>>& edges) {
+    vector<vector<pair<V, int>>> g(n_);
+    for (auto& e : edges) {
+      V u, v; int w; tie(u, v, w) = e;
+      g[u].emplace_back(v, w);
+      g[v].emplace_back(u, w);
+    }
 
-  void build_depth() {
-    depth_.resize(sz(binary_tree_edges_), -1);
-    parent_weight_.resize(sz(binary_tree_edges_), make_pair(-2, -2));
+    depth_.resize(n_, -1);
+    parent_weight_.resize(n_, make_pair(-2, -2));
 
-    FOR(v, num_vertices_) {
+    FOR(v, n_) {
       if (depth_[v] >= 0) continue;
 
       depth_[v] = 0;
@@ -76,7 +64,7 @@ class random_contraction {
       q.push(v);
       while (!q.empty()) {
         int u = q.front(); q.pop();
-        for (auto& to_weight : binary_tree_edges_[u]) {
+        for (auto& to_weight : g[u]) {
           int to, weight; tie(to, weight) = to_weight;
           if (depth_[to] >= 0) continue;
           depth_[to] = depth_[u] + 1;
@@ -89,9 +77,130 @@ class random_contraction {
     }
   }
 
+public:
+  tree_minimum_query() = default;
+  tree_minimum_query(const vector<tuple<V, V, int>>& edges, int num_vs) : n_(num_vs) {
+    build(edges);
+  }
+
+  int query(V u, V v) const {
+    CHECK(u != v);
+    CHECK(u < n_ && v < n_);
+    int ans = numeric_limits<int>::max();
+    while (u != v) {
+      if (u == -1 || v == -1) return 0; // disconnect
+      if (depth_[u] > depth_[v]) swap(u, v);
+      ans = min(ans, parent_weight_[v].second);
+      v = parent_weight_[v].first;
+    }
+    return ans;
+  }
+private:
+  int n_;
+  vector<pair<V, int>> parent_weight_;
+  vector<int> depth_;
+};
+
+class tree_minimum_querys {
+  using uvcost_t = tuple<V, V, int>;
+
+  vector<uvcost_t> remove_unfocused_edges(vector<uvcost_t>&& random_contraction_edges) {
+    //辺を重い順にsort
+    sort(random_contraction_edges.begin(), random_contraction_edges.end(), [](const uvcost_t& l, const uvcost_t& r) {
+      return get<2>(l) > get<2>(r);
+    });
+
+    //最大indexをunion findで使う頂点数に使う
+    auto get_max_vertex_index = [](vector<uvcost_t>& v) {
+      int ret = 0;
+      for (auto& e : v) ret = max(ret, max(get<0>(e), get<1>(e)));
+      return ret;
+    };
+    const int num_vertices = get_max_vertex_index(random_contraction_edges) + 1;
+
+    struct weight_t {
+      int w;
+      weight_t() : w(0) {}
+      weight_t(int w) : w(w) {}
+      bool operator<(const weight_t& r) const {
+        return w < r.w;
+      }
+      weight_t& operator+=(const weight_t& r) {
+        w = max(w, r.w);
+        return *this;
+      }
+      weight_t operator+(const weight_t& r) const {
+        weight_t ret = *this;
+        return ret += r;
+      }
+    };
+
+    weighted_union_find<weight_t> wuf(num_vertices);
+    //頂点を重くして、可能な限り焦点を当てている頂点がrootになるように
+    FOR(i, focused_num_) wuf.add_weight(i, weight_t(1));
+
+    vector<uvcost_t> ret;
+    for (auto& e : random_contraction_edges) {
+      V u, v; int w; tie(u, v, w) = e;
+      u = wuf.root(u); v = wuf.root(v);
+      // if (u == v) continue; 木なので、既に繋がっている辺はこない
+      wuf.unite(u, v);
+      if (u < focused_num_ && v < focused_num_) {
+        // 新しいgomory-hu treeの辺
+        // 辺は重い順にsortされているので、この辺で新しく頂点対が繋がった
+        //  = gomory_hu treeのedgeの重さは w
+        ret.emplace_back(u, v, w);
+      }
+    }
+
+    return ret;
+  }
+
+public:
+  // 注目する頂点数
+  // random contracionで新しく追加した頂点に対するクエリは飛んでこないので無視出来る
+  tree_minimum_querys(int focused_num) : focused_num_(focused_num) {}
+
+  void add_tree(vector<tuple<V, V, int>> random_contraction_edges) {
+    //仮の頂点を削除
+    random_contraction_edges = remove_unfocused_edges(std::move(random_contraction_edges));
+    tmqs_.emplace_back(random_contraction_edges, focused_num_);
+  }
+
+  int query(int u, int v) const {
+    int ans = numeric_limits<int>::max();
+    for (const auto& tmq : tmqs_) {
+      ans = min(ans, tmq.query(u, v));
+    }
+    return ans;
+  }
+private:
+  const int focused_num_;
+  vector<tree_minimum_query> tmqs_;
+};
+
+class random_contraction {
+  struct degree_weight {
+    int degree, weight;
+    degree_weight() : degree(0), weight(0) {}
+    degree_weight(int degree, int weight) : degree(degree), weight(weight) {}
+    bool operator<(const degree_weight& r) const {
+      return degree < r.degree;
+    }
+    degree_weight& operator+=(const degree_weight& r) {
+      degree += r.degree;
+      weight += r.weight;
+      return *this;
+    }
+    degree_weight operator+(const degree_weight& r) const {
+      degree_weight ret = *this;
+      return ret += r;
+    }
+  };
+
   void contraction(vector<int>& ancestor, vector<unordered_map<V, int>>& contraction_graph_edges, V u, V v, queue<int>& degree_eq_1) {
     assert(!uf_.is_same(u, v));
-    V new_vertex = (V)binary_tree_edges_.size();
+    const V new_vertex = num_vertices_ + sz(binary_tree_edges_) / 2;
     u = uf_.root(u), v = uf_.root(v);
     degree_weight uw = uf_.weight(u), vw = uf_.weight(v);
     uf_.unite(u, v);
@@ -104,11 +213,8 @@ class random_contraction {
     ancestor[u] = new_vertex;
 
     // 縮約した結果を元にbinary treeの構築を進める
-    binary_tree_edges_.emplace_back(); // new_vertex分の確保
-    binary_tree_edges_[new_vertex].emplace_back(ua, uw.weight);
-    binary_tree_edges_[ua].emplace_back(new_vertex, uw.weight);
-    binary_tree_edges_[new_vertex].emplace_back(va, vw.weight);
-    binary_tree_edges_[va].emplace_back(new_vertex, vw.weight);
+    binary_tree_edges_.emplace_back(ua, new_vertex, uw.weight);
+    binary_tree_edges_.emplace_back(va, new_vertex, vw.weight);
 
     //縮約した頂点の辺をまとめる
     auto& uset = contraction_graph_edges[u];
@@ -141,8 +247,8 @@ public:
   // g の辺を使い、グラフを作成する(勝手にundirectedとして読み替えている)
   random_contraction(G& g) :
     num_vertices_(g.num_vertices()),
-    uf_(g.num_vertices()),
-    binary_tree_edges_(g.num_vertices()) {
+    uf_(g.num_vertices()) {
+
     //unordered -> weight = 1なので、shuffleでよい
     //重みがあるならBITで。(stochastic acceptanceはupdateありだと使えない)
     vector<unordered_map<V, int>> contraction_graph_edges(g.num_vertices());
@@ -177,7 +283,7 @@ public:
           tie(u, std::ignore) = *contraction_graph_edges[v].begin();
           contraction(ancestor, contraction_graph_edges, u, v, degree_eq_1);
           if (contraction_graph_edges[u].size() == 1) degree_eq_1.push(u);
-          
+
           pruned_degree_eq_1++;
         }
       }
@@ -187,85 +293,43 @@ public:
       contraction(ancestor, contraction_graph_edges, u, v, degree_eq_1);
     }
     // gが連結とは限らないので、uv_costs.size() == g.num_vertices() - 1ではない
-    assert(int(binary_tree_edges_.size() - g.num_vertices()) <= g.num_vertices() - 1);
-
-    build_depth();
+    assert(sz(binary_tree_edges_) <= 2 * (num_vertices_ - 1));
   }
 
-  int query(V u, V v) const {
-    CHECK(u != v);
-    CHECK(u < num_vertices_ && v < num_vertices_);
-    int ans = numeric_limits<int>::max();
-    while (u != v) {
-      if (u == -1 || v == -1) return 0; // disconnect
-      if (depth_[u] > depth_[v]) {
-        ans = min(ans, parent_weight_[u].second);
-        u = parent_weight_[u].first;
-      } else {
-        ans = min(ans, parent_weight_[v].second);
-        v = parent_weight_[v].first;
-      }
-    }
-    return ans;
+  tree_minimum_querys build_solver() const {
+    tree_minimum_querys solver(num_vertices_);
+    solver.add_tree(binary_tree_edges_);
+    return solver;
   }
 
-  void single_source_mincut_dfs(V v, V par, int mn, vector<int>& ans) const {
-    if (v < num_vertices_) {
-      ans[v] = mn;
-    }
-    for (auto& to : binary_tree_edges_[v]) {
-      if (to.first == par) continue;
-      int nmn = min(mn, to.second);
-      single_source_mincut_dfs(to.first, v, nmn, ans);
-    }
-  }
-
-
-  vector<int> single_source_mincut(V u) const {
-    vector<int> ans(num_vertices_, numeric_limits<int>::max());
-    single_source_mincut_dfs(u, -1, numeric_limits<int>::max(), ans);
-    return ans;
+  //破壊的操作
+  vector<tuple<V, V, int>>&& edges() {
+    return move(binary_tree_edges_);
   }
 
 private:
   const int num_vertices_;
-  weighted_union_find uf_;
-
-  vector<vector<pair<V, int>>> binary_tree_edges_;
-  vector<pair<V, int>> parent_weight_;
-  vector<int> depth_;
+  weighted_union_find<degree_weight> uf_;
+  vector<tuple<V, V, int>> binary_tree_edges_;
 };
 
 class min_cut_query {
 public:
-  min_cut_query(G& g) {
+  min_cut_query(G& g) : tmq_(g.num_vertices()) {
     fprintf(stderr, "initialize mincut-query tree ...\n");
     for (int i = 0; i < FLAGS_solver_iter; i++) {
       if (i % 10 == 0) fprintf(stderr, "trees .. %d/%d\n", i, FLAGS_solver_iter);
-      solvers_.emplace_back(g);
+      random_contraction rc(g);
+      tmq_.add_tree(rc.edges());
     }
     fprintf(stderr, "completed.\n");
   }
 
   int query(int u, int v) const {
-    int ans = numeric_limits<int>::max();
-    for (const auto& solver : solvers_) {
-      ans = min(ans, solver.query(u, v));
-    }
+    int ans = tmq_.query(u, v);
     return ans;
   }
 
-  vector<int> single_source_mincut(V v) const {
-    vector<int> ret;
-    for (const auto& solver : solvers_) {
-      auto cur_ans = solver.single_source_mincut(v);
-      if (sz(ret) == 0) ret = move(cur_ans);
-      else {
-        FOR(i, sz(ret)) ret[i] = min(ret[i], cur_ans[i]);
-      }
-    }
-    return ret;
-  }
 private:
-  vector<random_contraction> solvers_;
+  tree_minimum_querys tmq_;
 };
