531468e1e2c5d2dc9b352d25ee6888750169360d
kenkoooo <kenkou.n@gmail.com>
2016-03-15 21:13:37 +0900

    Fix vertex size

--- playground/fractal_visualizer.py
+++ playground/fractal_visualizer.py
@@ -36,7 +36,7 @@ def getMergedList(G, centers_list, rad):
             merged[u] = merged[q]
             queue.append(u)
             cnt += 1
-            if cnt % 1000 == 0:
+            if cnt % 5000 == 0:
                 print cnt
     return merged
 
@@ -46,11 +46,13 @@ def shrinkGraph(G, centers_list, rad, need_pos=False):
     covered_size = [0] * nx.number_of_nodes(G)
     for m in merged:
         covered_size[m] += 1
-    p = pd.DataFrame(G.edges())
     shrinked_edges = []
-    for i, row in p.iterrows():
+    for row in G.edges():
         s = row[0]
         t = row[1]
+        if s >= len(merged) or t >= len(merged):
+            print s, t, len(merged), G.number_of_nodes()
+            continue
         if merged[s] == merged[t]:
             continue
         shrinked_edges.append((merged[s], merged[t]))
@@ -95,7 +97,7 @@ if __name__ == "__main__":
         nx.draw_networkx_nodes(shrinked_G,
                                pos,
                                nodelist=shrinked_G.nodes(),
-                               node_size=[covered_size[v] / 5 for v in shrinked_G.nodes()])
+                               node_size=[covered_size[v] / 1000 for v in shrinked_G.nodes()])
         nx.draw_networkx_edges(shrinked_G, pos, width=0.1)
 
         pylab.xlim(-1.1, 1.1)
