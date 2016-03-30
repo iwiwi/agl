fa3563e6fd617f929198fc3792a093b2104f947e
math <syousuke@nii.ac.jp>
2016-03-30 20:52:03 +0900

    two_edge_CC_filterをきれいにした

--- playground/two_edge_CC_filter.h
+++ playground/two_edge_CC_filter.h
@@ -14,19 +14,19 @@ public:
       if (to == par) continue;
       if (order_[to] == -1) {
         lowlink_dfs(to, v, cur_ord);
-        lowlink_[v] = min(lowlink_[v], lowlink_[to]);
+        lowlink_[v] = std::min(lowlink_[v], lowlink_[to]);
         if (order_[v] < lowlink_[to]) bridge_.emplace_back(v, to);
         else biconnected_graphs_edges_.emplace_back(v, to);
       } else {
-        lowlink_[v] = min(lowlink_[v], lowlink_[to]);
+        lowlink_[v] = std::min(lowlink_[v], lowlink_[to]);
         if (v < to) biconnected_graphs_edges_.emplace_back(v, to);
       }
     }
   }
 
-  vector<vector<int>> get_local_id2global_id() const {
+  std::vector<std::vector<int>> get_local_id2global_id() const {
     const int cc_size = int(biconnected_graph_handler_->handlers().size());
-    vector<vector<int>> local_id2global_id(cc_size);
+    std::vector<std::vector<int>> local_id2global_id(cc_size);
 
     const auto& local_indices = biconnected_graph_handler_->local_indices();
     for(int v = 0; v < n_; v++) {
@@ -41,31 +41,27 @@ public:
   two_edge_CC_filter(G& g) : n_(g.num_vertices()), g_(g), uf_(n_), lowlink_(n_, -1), order_(n_, -1) {
 
     G new_g;
-    fprintf(stderr, "two_edge_CC_filter::constructor start : memory %ld MB\n", jlog_internal::get_memory_usage() / 1024);
-    JLOG_ADD_BENCHMARK("time.decompose_2_connected_components") {
-      for(int v = 0; v < n_; v++) for (auto& e : g_.edges(v)) {
-        uf_.unite(v, to(e));
-      }
-
-      const int num_edges = g_.num_edges();
-
-      for(int v = 0; v < n_; v++) if (uf_.root(v) == v) {
-        int cur_ord = 0;
-        lowlink_dfs(v, -1, cur_ord);
-      }
-
-      CHECK(bridge_.size() + biconnected_graphs_edges_.size() == size_t(num_edges));
-
-      g_.clear_and_shrink_to_fit();
-      new_g = G(biconnected_graphs_edges_, n_);
-
-      //dealloc
-      lowlink_.clear(); lowlink_.shrink_to_fit();
-      order_.clear(); order_.shrink_to_fit();
-      // bridge_.clear(); bridge_.shrink_to_fit();
-      biconnected_graphs_edges_.clear(); biconnected_graphs_edges_.shrink_to_fit();
+    for(int v = 0; v < n_; v++) for (auto& e : g_.edges(v)) {
+      uf_.unite(v, to(e));
     }
-    fprintf(stderr, "two_edge_CC_filter::constructor end, before connected_components_filter : memory %ld MB\n", jlog_internal::get_memory_usage() / 1024);
+
+    const int num_edges = g_.num_edges();
+
+    for(int v = 0; v < n_; v++) if (uf_.root(v) == v) {
+      int cur_ord = 0;
+      lowlink_dfs(v, -1, cur_ord);
+    }
+
+    CHECK(bridge_.size() + biconnected_graphs_edges_.size() == std::size_t(num_edges));
+
+    g_.clear_and_shrink_to_fit();
+    new_g = G(biconnected_graphs_edges_, n_);
+
+    //dealloc
+    lowlink_.clear(); lowlink_.shrink_to_fit();
+    order_.clear(); order_.shrink_to_fit();
+    // bridge_.clear(); bridge_.shrink_to_fit();
+    biconnected_graphs_edges_.clear(); biconnected_graphs_edges_.shrink_to_fit();
 
     biconnected_graph_handler_.reset(new connected_components_filter<handler_t>(new_g));
   }
@@ -80,13 +76,13 @@ public:
     return ans;
   }
 
-  void print_gomory_hu_tree(ostream& os) {
-    vector<int> roots;
+  void print_gomory_hu_tree(std::ostream& os) {
+    std::vector<int> roots;
     for(int v = 0; v < n_; v++) if (uf_.root(v) == v) roots.push_back(v);
     for(int i = 0; i < int(roots.size()) - 1; i++) os << roots[0] << " " << roots[i + 1] << " 0\n";
     for (auto& e : bridge_) os << e.first << " " << e.second << " 1\n";
 
-    vector<vector<int>> local_id2global_id = get_local_id2global_id();
+    std::vector<std::vector<int>> local_id2global_id = get_local_id2global_id();
 
     //weight2以上
     for(int i = 0; i < int(biconnected_graph_handler_->handlers().size()); i++) {
@@ -107,9 +103,9 @@ private:
   const int n_;
   G& g_;
   union_find uf_;
-  vector<int> lowlink_, order_;
-  vector<pair<V, V>> bridge_, biconnected_graphs_edges_;
+  std::vector<int> lowlink_, order_;
+  std::vector<std::pair<V, V>> bridge_, biconnected_graphs_edges_;
 
-  unique_ptr<connected_components_filter<handler_t>> biconnected_graph_handler_;
+  std::unique_ptr<connected_components_filter<handler_t>> biconnected_graph_handler_;
 };
 } //namespace agl
