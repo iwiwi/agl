267e1529def0b8679a7009f6055369357445d1c8
Naoto Mizuno <nmizuno@nii.ac.jp>
2016-04-05 15:31:41 +0900

    all pairs distance

--- src/shortest_path/dijkstra.h
+++ src/shortest_path/dijkstra.h
@@ -20,5 +20,17 @@ std::vector<typename GraphT::W> single_source_distance(const GraphT &g, V s, D d
   return h.weights();
 }
 
+template<typename GraphT = G>
+std::vector<std::vector<typename GraphT::W>> all_pairs_distance(const GraphT &g, D d = kFwd) {
+  using W = typename GraphT::W;
+  std::vector<std::vector<W>> dm;
+
+  for (const auto& s : g.vertices()) {
+    dm.emplace_back(single_source_distance(g, s, d));
+  }
+
+  return dm;
+}
+
 std::vector<std::pair<W, double>> single_source_distance_with_num_paths(const G &g, V s, D d = kFwd);
 }  // namespace agl
--- src/shortest_path/dijkstra_test.cc
+++ src/shortest_path/dijkstra_test.cc
@@ -33,6 +33,40 @@ TEST(dijkstra, weighted) {
   cout << endl;
 }
 
+TEST(all_pairs_distance, unweighted) {
+  G g(G::edge_list_type({
+          {0, {1}},
+          {1, {2}},
+          {2, {3}},
+          {3, {1}},
+  }));
+
+  auto dm = all_pairs_distance(g);
+  for (const auto& v : g.vertices()) {
+    auto ds = single_source_distance(g, v);
+    for (V u : irange<V>(g.num_vertices())) {
+      ASSERT_EQ(dm[v][u], ds[u]);
+    }
+  }
+}
+
+TEST(all_pairs_distance, weighted) {
+  weighted_graph<> g(weighted_graph<>::edge_list_type{
+          {0, {1, 1.5}},
+          {1, {2, 20}},
+          {2, {3, 5}},
+          {3, {1, 0.01}},
+  });
+
+  auto dm = all_pairs_distance(g);
+  for (const auto& v : g.vertices()) {
+    auto ds = single_source_distance(g, v);
+    for (V u : irange<V>(g.num_vertices())) {
+      ASSERT_EQ(dm[v][u], ds[u]);
+    }
+  }
+}
+
 namespace {
 int fac(int k) {
   return k == 0 ? 1 : k * fac(k - 1);
