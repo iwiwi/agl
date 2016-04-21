656cd05dc7989cbeeb3649c62ca063dd990e20f6
Naoto Mizuno <nmizuno@nii.ac.jp>
2016-04-21 16:27:53 +0900

    weighted graph

--- src/shortest_path/diameter.h
+++ src/shortest_path/diameter.h
@@ -2,11 +2,14 @@
 
 #include <stack>
 
-#include "graph/graph.h"
+#include "dijkstra.h"
 
 namespace agl {
-template<int kNumDoubleSweep = 10>
-int diameter(const G& g) {
+template<typename GraphT = G, int kNumDoubleSweep = 10>
+typename GraphT::W diameter(const GraphT& g) {
+  using W = typename GraphT::W;
+  constexpr W INF = std::numeric_limits<W>::max();
+  auto cmp = [](W a, W b){return (a != INF ? a : -1) < (b != INF ? b : -1);};
   V num_v = g.num_vertices();
 
   // Decompose the graph into strongly connected components
@@ -24,7 +27,7 @@ int diameter(const G& g) {
       dfs.emplace(i, -1);
 
       while (!dfs.empty()) {
-        int v = dfs.top().first;
+        V v = dfs.top().first;
         int index = dfs.top().second;
         dfs.pop();
         if (index == -1) {
@@ -32,11 +35,11 @@ int diameter(const G& g) {
           s.emplace(v);
           in[v] = true;
         } else {
-          low[v] = std::min(low[v], low[to(g.neighbor(v, index))]);
+          low[v] = std::min(low[v], low[to(g.edge(v, index))]);
         }
 
         for (++index; index < g.degree(v); ++index) {
-          V w = to(g.neighbor(v, index));
+          V w = to(g.edge(v, index));
           if (ord[w] == -1) {
             dfs.emplace(v, index);
             dfs.emplace(w, -1);
@@ -60,57 +63,15 @@ int diameter(const G& g) {
     }
   }
 
-  // Compute the diameter lower bound by the double sweep algorithm
-  int diameter = 0;
-  int qs, qt;
-  std::vector<int> dist(num_v, -1);
-  std::vector<V> queue(num_v);
-  {
-    for (int i = 0; i < kNumDoubleSweep; ++i) {
-      // forward BFS
-      V start = agl::random(num_v);
-      qs = qt = 0;
-      dist[start] = 0;
-      queue[qt++] = start;
-      while (qs < qt) {
-        V v = queue[qs++];
-        for (const auto& e : g.neighbors(v)) {
-          if (dist[to(e)] < 0) {
-            dist[to(e)] = dist[v] + 1;
-            queue[qt++] = to(e);
-          }
-        }
-      }
-      for (int j : make_irange(qt)) dist[queue[j]] = -1;
-
-      // barkward BFS
-      start = queue[qt - 1];
-      qs = qt = 0;
-      dist[start] = 0;
-      queue[qt++] = start;
-      while (qs < qt) {
-        V v = queue[qs++];
-        for (const auto& e : g.neighbors(v, kBwd)) {
-          if (dist[to(e)] < 0) {
-            dist[to(e)] = dist[v] + 1;
-            queue[qt++] = to(e);
-          }
-        }
-      }
-      diameter = std::max(diameter, dist[queue[qt - 1]]);
-      for (int j : make_irange(qt)) dist[queue[j]] = -1;
-    }
-  }
-
   // Order vertices
   std::vector<std::pair<int64_t, V>> order(num_v);
   {
     for (V v : make_irange(num_v)) {
       int in = 0, out = 0;
-      for (const auto& e : g.neighbors(v, kBwd)) {
+      for (const auto& e : g.edges(v, kBwd)) {
         if (scc[to(e)] == scc[v]) ++in;
       }
-      for (const auto& e : g.neighbors(v)) {
+      for (const auto& e : g.edges(v)) {
         if (scc[to(e)] == scc[v]) ++out;
       }
       // SCC : reverse topological order
@@ -120,22 +81,38 @@ int diameter(const G& g) {
     std::sort(order.begin(), order.end());
   }
 
-  // Examine every vertex
-  std::vector<int> ecc(num_v, num_v);
+  // Compute the diameter lower bound by the double sweep algorithm
+  W diameter = 0;
   {
-    for (V i : make_irange(num_v)) {
+    for (int i = 0; i < kNumDoubleSweep; ++i) {
+      V start = agl::random(num_v);
+      auto dist = single_source_distance(g, start);
+      start = max_element(dist.begin(), dist.end(), cmp) - dist.begin();
+      dist = single_source_distance(g, start);
+      diameter = std::max(diameter, *max_element(dist.begin(), dist.end(), cmp));
+    }
+  }
+
+  // Examine every vertex
+  std::vector<W> ecc(num_v, INF);
+  {
+    for (int i : make_irange(num_v)) {
       V u = order[i].second;
       if (ecc[u] <= diameter) continue;
 
       // Refine the eccentricity upper bound
-      int ub = 0;
-      std::vector<std::pair<int, int>> neighbors;
-      for (const auto& e : g.neighbors(u)) neighbors.emplace_back(scc[to(e)], ecc[to(e)] + 1);
+      W ub = 0;
+      std::vector<std::pair<int, W>> neighbors;
+      for (const auto& e : g.edges(u)) {
+        if (ecc[to(e)] != INF) {
+          neighbors.emplace_back(scc[to(e)], ecc[to(e)] + weight(e));
+        }
+      }
       sort(neighbors.begin(), neighbors.end());
 
       for (size_t j = 0; j < neighbors.size(); ) {
         int component = neighbors[j].first;
-        int lb = num_v;
+        W lb = INF;
         for (; j < neighbors.size(); ++j) {
           if (neighbors[j].first != component) break;
           lb = std::min(lb, neighbors[j].second);
@@ -149,38 +126,24 @@ int diameter(const G& g) {
         continue;
       }
 
-      // Conduct a BFS and update bounds
-      qs = qt = 0;
-      dist[u] = 0;
-      queue[qt++] = u;
-      while (qs < qt) {
-        int v = queue[qs++];
-        for (const auto& e : g.neighbors(v)) {
-          if (dist[to(e)] < 0) {
-            dist[to(e)] = dist[v] + 1;
-            queue[qt++] = to(e);
-          }
-        }
-      }
-      ecc[u] = dist[queue[qt - 1]];
+      // Update bounds
+      auto dist = single_source_distance(g, u);
+      ecc[u] = *max_element(dist.begin(), dist.end(), cmp);
       diameter = std::max(diameter, ecc[u]);
-      for (int j : make_irange(qt)) dist[queue[j]] = -1;
 
-      qs = qt = 0;
-      dist[u] = 0;
-      queue[qt++] = u;
-      while (qs < qt) {
-        V v = queue[qs++];
-        ecc[v] = std::min(ecc[v], dist[v] + ecc[u]);
-        for (const auto& e : g.neighbors(v, kBwd)) {
-          // only inside an SCC
-          if (dist[to(e)] < 0 && scc[to(e)] == scc[u]) {
-            dist[to(e)] = dist[v] + 1;
-            queue[qt++] = to(e);
+      auto h = make_dijkstra_heap(g);
+      h.decrease(u, 0);
+      while (!h.empty()) {
+        V v = h.top_vertex();
+        W w = h.top_weight();
+        h.pop();
+        ecc[v] = std::min(ecc[v], w + ecc[u]);
+        for (const auto &e : g.edges(v, kBwd)) {
+          if (scc[to(e)] == scc[u]) {
+            h.decrease(to(e), w + weight(e));
           }
         }
       }
-      for (int j : make_irange(qt)) dist[queue[j]] = -1;
     }
   }
 
