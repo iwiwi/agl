c7e993fe3256873b729d7310c1b387558f0b740e
Naoto Mizuno <nmizuno@nii.ac.jp>
2016-04-26 11:48:03 +0900

    improve logging

--- src/shortest_path/diameter_main.cc
+++ src/shortest_path/diameter_main.cc
@@ -3,9 +3,16 @@
 
 int main(int argc, char** argv) {
   G g = easy_cui_init(argc, argv);
-  
+  V d;
+
   JLOG_PUT_BENCHMARK("diameter") {
-    printf("diameter = %d\n", diameter(g));
+    d = diameter(g);
+  }
+
+  JLOG_OPEN("graph_info") {
+    JLOG_PUT("vertices", g.num_vertices());
+    JLOG_PUT("edges", g.num_edges());
+    JLOG_PUT("diameter", d);
   }
 
   return 0;
